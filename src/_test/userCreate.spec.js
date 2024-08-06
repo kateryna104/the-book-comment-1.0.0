ddb738a0168443762b698e4c2a25ac86b5eeb8db
Your Name <you@example.com>
2024-08-06 16:58:30 -0400

    created first_test

---
+++ src/_test/userCreate.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('USER CREATE', () => {
+    describe('USER CREATE - POSITIVE TESTS', () => {
+      it('user create all fields' , () => {
+
+      })
+    })
+
+    describe('USER CREATE - NEGATIVE TESTS', () => {
+
+    })
+
+})
