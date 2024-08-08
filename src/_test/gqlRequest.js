9fa98ab8b11f5e3c7e647099ba878598dc901b55
Your Name <you@example.com>
2024-08-07 23:33:32 -0400

    one more

--- src/_test/gqlRequest.js
+++ src/_test/gqlRequest.js
@@ -1,9 +1,11 @@
 const request  = require('supertest');
 
-const url = 'http://localhost:5000/'
+const url = 'http://localhost:5000'
 
 function gqlRequest (postData) {
-    return request(url).post('/').send(postData);
+    return request(url)
+        .post('/')
+        .send(postData);
 }
 
 module.exports = gqlRequest
