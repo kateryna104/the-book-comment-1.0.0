d03e046c9b1d912b46f583c4c091f7407576e15b
Your Name <you@example.com>
2024-08-15 19:50:58 -0400

    crested user2Create

--- src/_test/user2/user2Create.spec.js
+++ src/_test/user2/user2Create.spec.js
@@ -1,9 +1,30 @@
-const request = require( 'supertest')
+
 const { expect } = require( 'chai' )
+const { user2CreateQ } = require( './queries2' )
+const { user2 } = require( './data2')
+const gqlRequest = require('../gqlRequest')
+
+
+let respData = null;
+let postData = null;
 
 describe( 'USER CREATE', () => {
     describe( 'USER CREATE - POSITIVE TESTS', () => {
-        it( 'user create all fields', () =>{
+        it( 'user create all fields', (done ) =>{
+            postData = {
+                query: user2CreateQ,
+                variables: user2
+            }
+            gqlRequest( postData )
+                .expect(200)
+                .end((err, res) =>{
+                    if (err) return done (err)
+                    respData = res.body
+                    console.log(respData);
+                    expect(respData).eq()
+                    done()
+
+                })
 
         })
         it( 'user create all fields', () =>{
