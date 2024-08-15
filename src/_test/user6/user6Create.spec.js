04112fe2ebf96f81e10244c94c6ffc9a2a829bca
Your Name <you@example.com>
2024-08-15 16:09:38 -0400

    created new direcories users

---
+++ src/_test/user6/user6Create.spec.js
@@ -0,0 +1,29 @@
+const request = require( 'supertest')
+const { expect} = require( 'chai')
+
+describe( 'USER CREATE', () => {
+    describe(' USER CREATE - POSITIVE TESTS', () => {
+        it ( ' user create all fields', () =>{
+
+        })
+        it ( ' user create all fields', () =>{
+
+        })
+        it ( ' user create all fields', () =>{
+
+        })
+    })
+    describe ('USER CREATE - NEGATiVE TESTS', () => {
+        it ( ' user create all fields', () =>{
+
+        })
+        it ( ' user create all fields', () =>{
+
+        })
+        it ( ' user create all fields', () =>{
+
+        })
+
+    })
+
+})
